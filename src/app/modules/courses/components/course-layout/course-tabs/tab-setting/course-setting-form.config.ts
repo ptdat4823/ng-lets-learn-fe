f9e71ed014207604d32a692754e5590f8a73aafa
nukota <22521351@gm.uit.edu.vn>
2025-06-12 17:51:56 +0700

    update course-setting-tab

--- src/app/modules/courses/components/course-layout/course-tabs/tab-setting/course-setting-form.config.ts
+++ src/app/modules/courses/components/course-layout/course-tabs/tab-setting/course-setting-form.config.ts
@@ -3,8 +3,9 @@ import { FormControlField } from '@shared/helper/form.helper';
 
 export const courseSettingFormSchema = {
   name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-  description: new FormControl(''),
-  content: new FormControl(''),
+  category: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  level: new FormControl('beginner', [Validators.required]),
+  price: new FormControl('', [Validators.required, Validators.min(0)])
 };
 
 export const courseGeneralSettingFormControls: FormControlField[] = [
@@ -26,9 +27,50 @@ export const courseGeneralSettingFormControls: FormControlField[] = [
     componentType: 'input',
     placeholder: 'Astronomy',
     validationMessages: {
-      required: 'Page name is required',
-      minlength: 'Page name must be at least 2 characters',
+      required: 'Category is required',
+      minlength: 'Category must be at least 2 characters',
+    },
+  },
+  {
+    id: 'level',
+    label: 'Level',
+    type: 'text',
+    componentType: 'select',
+    placeholder: 'Astronomy',
+    options: [
+      { value: 'beginner', label: 'Beginner' },
+      { value: 'intermediate', label: 'Intermediate' },
+      { value: 'advanced', label: 'Advanced' },
+    ],
+    validationMessages: {
+      required: 'Level name is required',
+    },
+  },
+  {
+    id: 'price',
+    label: 'Price',
+    type: 'number',
+    componentType: 'input',
+    placeholder: '0',
+    validationMessages: {
+      required: 'Price is required',
+      min: 'Price must be a positive number',
     },
   },
 ];
-export const pageDangerZoneSettingFormControls: FormControlField[] = [];
+export const courseDangerZoneSettingFormControls: FormControlField[] = [
+  {
+    id: 'change-to-public',
+    label: 'Change course visibility',
+    type: 'button',
+    componentType: 'button',
+    placeholder: '',
+    note: 'This course is currently private.',
+    buttonColor: '#B91C1C',
+    buttonText: 'Change to public',
+    onClick: () => {
+      console.log('Change visibility action triggered');
+    },
+    validationMessages: {},
+  },
+];
--- src/app/modules/courses/components/course-layout/course-tabs/tab-setting/tab-setting.component.ts
+++ src/app/modules/courses/components/course-layout/course-tabs/tab-setting/tab-setting.component.ts
@@ -1,4 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CollapsibleListService } from '@shared/components/collapsible-list/collapsible-list.service';
+import {
+  courseGeneralSettingFormControls,
+  courseDangerZoneSettingFormControls,
+  courseSettingFormSchema,
+} from './course-setting-form.config';
 import { Course } from '@shared/models/course';
 
 @Component({
@@ -6,8 +13,60 @@ import { Course } from '@shared/models/course';
   standalone: false,
   templateUrl: './tab-setting.component.html',
   styleUrl: './tab-setting.component.scss',
+  providers: [CollapsibleListService],
 })
 export class TabSettingComponent implements OnInit {
   @Input({ required: true }) course!: Course;
-  ngOnInit(): void {}
+  sectionIds: string[] = ['general', 'danger-zone'];
+  showPassword = false;
+  form!: FormGroup;
+  generalFormControls = courseGeneralSettingFormControls;
+  dangerZoneFormControls = courseDangerZoneSettingFormControls;
+
+  constructor(
+    private fb: FormBuilder,
+    private collapsibleListService: CollapsibleListService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group(courseSettingFormSchema, { updateOn: 'submit' });
+    this.collapsibleListService.setSectionIds(this.sectionIds);
+    this.collapsibleListService.setCanEdit(false); // disable edit UI in collapsible list
+    this.collapsibleListService.expandAll();
+  }
+
+  getDisabled(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    console.log(controlName, control);
+    return control ? control.disabled : false;
+  }
+
+  onSubmit(e: Event): void {
+    e.preventDefault(); // Prevent default form submission
+    // Stop here if form is invalid
+    if (this.form.invalid) {
+      this.form.markAllAsTouched(); // Mark all controls as touched to show validation errors
+
+      // Find first invalid control and scroll to it
+      const firstInvalidControl: HTMLElement = document.querySelector(
+        'form .ng-invalid'
+      ) as HTMLElement;
+
+      if (firstInvalidControl) {
+        firstInvalidControl.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        firstInvalidControl.focus();
+      }
+
+      return;
+    }
+
+    // Here you would typically call your authentication service
+    console.log('submit attempt with:', this.form.value);
+
+    // Navigate to dashboard or home page after successful login
+    // this.router.navigate(['/dashboard']);
+  }
 }
